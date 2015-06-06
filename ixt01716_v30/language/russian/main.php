f5a6f5a176f048f0a880560398f48ddb65c583d5
mambax7 <mambax7@gmail.com>
2015-06-06 07:59:06 -0400

    Original files

---
+++ ixt01716_v30/language/russian/main.php
@@ -0,0 +1,27 @@
+<?php
+/*
+* Qualitative Themes for XOOPS
+*
+* @license       http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+* @project       IXThemes Project, http://ixthemes.org
+* @theme_name    ixt01716
+* @theme_release 3.0
+* @theme_demo    http://ixthemes.sourceforge.net/demo/x/index.php?xoops_theme_select=ixt01716
+* @theme_author  Alexander Galochkin (algalochkin at users.sourceforge.net)
+* @version       $Id: main.php 2780 2010-04-25 08:41:37Z algalochkin $
+*
+* Thanks you, that you have left this header untouched!!!
+*/
+define("THEME_NAME", "ixt01716 rel.#3.0");
+define("THEME_DESC", "Theme based on ixt01716 Theme by IXThemes");
+define("THEME_DESIGNER", "Design by : <a rel='external' rel='external' href='http://ixthemes.org/' title='IXThemes Project'>IXThemes</a>");
+define("THEME_UPTOP", "Back to Top");
+define("THEME_VALIDCSS", "This theme template is W3C Validated for CSS 1 and 2");
+define("THEME_VALIDXHTML", "This theme template is W3C Validated for XHTML 1.01");
+define("THEME_LICENSE", "See Creative Commons Licence");
+// control1.html
+define("THEME_SHOW", "показать");
+define("THEME_HIDE", "скрыть");
+// footer_ext.html
+define("THEME_DOWNLOAD", "загрузить");
+?>
